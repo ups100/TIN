f4d5b27ccf7f0bde87278bf09dee01322b9f87c1
Kajo0 <kajo100@gmail.com>
2012-12-18 23:12:46 +0100

    missing files

---
+++ code/Utilities/include/InterprocessName.h
@@ -0,0 +1,18 @@
+#if !defined(INTERPROCESS_NAME__INCLUDED_)
+#define INTERPROCESS_NAME__INCLUDED_
+
+namespace TIN_project {
+namespace Utilities {
+
+/**
+ * Contains interprocess socket name for Client - Daemon communication
+ */
+class InterprocessName
+{
+public:
+    static const char *INTERPROCESS_NAME;
+};
+
+} //namespace Utilities
+} //namespace TIN_project
+#endif // !defined(INTERPROCESS_NAME__INCLUDED_)
---
+++ code/Utilities/src/InterprocessName.cpp
@@ -0,0 +1,10 @@
+#include "InterprocessName.h"
+
+namespace TIN_project {
+namespace Utilities {
+
+/** Name for unix socket to client-daemon communication */
+const char *InterprocessName::INTERPROCESS_NAME = "TIN_project_inter_con";
+
+} //namespace Utilities
+} //namespace TIN_project
