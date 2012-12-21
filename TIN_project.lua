50ba809dc02a30a579385e23a0f23ca999ece56d
markubik <marcin@ubuntu.(none)>
2012-12-21 15:15:24 +0100

    Adding lua script

---
+++ TIN_project.lua
@@ -0,0 +1,27 @@
+TIN_project = Proto("Tin_project", "TIN_project protocol")
+local f = TIN_project.fields
+
+f.msgid = ProtoField.uint8 ("Tin_project.msgid", "Message Id")
+f.data = ProtoField.bytes("Tin_project.data", "Data")
+
+function TIN_project.dissector (buffer, pinfo, tree)
+
+	local subtree = tree:add(TIN_project, buffer())
+	local offset = 0
+	
+	local msgid = buffer(offset,1)
+	
+	pinfo.cols.protocol = "TIN_project"
+	pinfo.cols.info = "Message ID: "
+	pinfo.cols.info:append(msgid:uint())
+
+	subtree:add(f.msgid, msgid)
+	offset = offset + 1
+	subtree:add(f.data,buffer(offset))
+	
+end
+
+function TIN_project.init()
+end
+tcp_table = DissectorTable.get("tcp.port")
+tcp_table:add(8080,TIN_project)
