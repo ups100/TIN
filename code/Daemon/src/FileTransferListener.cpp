812db2fedd7ded656953a4a9a0adcf3139018bae
ups100 <ups100@tlen.pl>
2013-01-07 08:57:54 +0100

    FileSender class done, not tested yet

--- code/Daemon/src/FileTransferListener.cpp
+++ code/Daemon/src/FileTransferListener.cpp
@@ -1,9 +1,19 @@
-///////////////////////////////////////////////////////////
-//  FileTransferListener.cpp
-//  Implementation of the Class FileTransferListener
-//  Created on:      07-gru-2012 00:33:36
-//  Original author: kopasiak
-///////////////////////////////////////////////////////////
+/**
+ * @file FileTransferListener.cpp
+ *
+ * @date 05-01-2013
+ *
+ * @author Opasiak Krzsztof <ups100@tlen.pl>
+ *
+ * @brief Implementation of the Class TIN_project::Server::FileTransferServer
+ *
+ * @par Project
+ * This is a part of project realized on Warsaw University of Technology
+ * on TIN lectures. Project was created to simplify synchronization between catalogs,
+ * that are stored on different hosts to let clients work on the same files anywhere they want.
+ * Allows user to do operations such as searching, copying and distributing files
+ * gathered under one alias.
+ */
 
 #include "FileTransferListener.h"
 
